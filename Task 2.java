631d857dc699b9b98a662471f76fb1a276914d61
DevashishK26 <108808484+DevashishK26@users.noreply.github.com>
2023-05-24 10:56:45 +0530

    Add files via upload

---
+++ Task 2.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100) + 1;
+        int attempts = 0;
+        boolean guessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        while (!guessedCorrectly) {
+            System.out.print("Enter your guess (1-100): ");
+            int guess = scanner.nextInt();
+            attempts++;
+
+            if (guess == secretNumber) {
+                System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts!");
+                guessedCorrectly = true;
+            } else if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else {
+                System.out.println("Too high! Try again.");
+            }
+        }
+
+        scanner.close();
+    }
+}
